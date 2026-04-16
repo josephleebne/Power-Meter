2f87f015859fc7c841752d18ce262dce628e6208
Joseph Lee <s4883975@uq.edu.au>
2026-04-16 13:58:37 +1000

    Adding measurement project into repo (deleting old measurement.x and retrying)

---
+++ embedded/measurement.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: josephlee
+ *
+ * Created on April 16, 2026, 1:57 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
