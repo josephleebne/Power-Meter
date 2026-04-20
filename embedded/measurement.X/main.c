11b10e3e54825f8f0d030543a012f35f8a57fc06
Joseph Lee <s4883975@uq.edu.au>
2026-04-21 01:25:11 +1000

    Added DC Measurement Reading

--- embedded/measurement.X/main.c
+++ embedded/measurement.X/main.c
@@ -2,17 +2,26 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <avr/io.h>
+#include <math.h> //ASK!!!!!!
 
+//UART Communication
 #define F_CPU 8000000UL
 #define BAUD 9600
 #define UBRR_VALUE ((F_CPU / (16UL * BAUD)) - 1)
+
+//Measurements
+#define VREF 5.0f
+#define NUM_SAMPLES 100
+#define DISCARD_FIRST_SAMPLE 1
+#define AC_VOLTAGE_RATIO (680.0/3300.0)
+#define NO_MEASUREMENTS 2
 
 void UART_init(void) {
     UBRR0H = (unsigned char)(UBRR_VALUE >> 8);
     UBRR0L = (unsigned char)UBRR_VALUE;
 
     UCSR0B = (1 << TXEN0);
-    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+    UCSR0C = (1 << USBS0) | (1 << UCSZ01) | (1 << UCSZ00);
 }
 
 void UART_transmit(char data) {
@@ -33,27 +42,23 @@ void UART_print_uint(uint16_t value) {
     UART_print(buffer);
 }
 
-void UART_print_voltage_2dp(float value) {
+void UART_print_float_2dp(float value) {
     if (value < 0) {
         UART_print("-");
         value = -value;
     }
 
-    int whole = (int)value;
-    int frac = (int)((value - whole) * 100.0f + 0.5f);  // rounded to 2 dp
-
-    if (frac >= 100) {
-        whole += 1;
-        frac = 0;
-    }
+    uint16_t scaled = (uint16_t)(value * 100.0f + 0.5f);
+    uint16_t whole = scaled / 100;
+    uint16_t frac  = scaled % 100;
 
     char buffer[16];
-    snprintf(buffer, sizeof(buffer), "%d.%02d", whole, frac);
+    snprintf(buffer, sizeof(buffer), "%u.%02u", whole, frac);
     UART_print(buffer);
 }
 
 void setup_ADC(void) {
-    ADMUX = 0x00;   // AREF, right-adjusted, ADC0
+    ADMUX = (0 << REFS1) | (0 << REFS0) | (0 << ADLAR); // AREF, 10 bit results
     ADCSRA = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0) | (1 << ADEN);
 }
 
@@ -65,27 +70,80 @@ uint16_t read_ADC(void) {
 }
 
 float calculate_DC_voltage(uint16_t ADCreading) {
-    float scaledVout = ((float)ADCreading * 5.0f) / 1023.0f;
-    float bias = 1.65f;
-    float error = 0.188f;
-    float trueVout = (scaledVout - bias) / 0.1f + error;
-    return trueVout;
+    
+    float scalingRatio =  (4.724f / 10.0f);
+    
+    //Set convert ADC reading into voltage
+    float scaledVout = ((float)ADCreading * VREF) / 1023.0f;
+    
+    float Vin = scaledVout / scalingRatio;
+
+    return Vin;
+}
+
+float calculate_DC_current(uint16_t ADCreading) {
+    
+    float scalingRatio = 21;
+    
+    //Set convert ADC reading into voltage
+    float scaledVout = ((float)ADCreading * VREF) / 1023.0f;
+    
+    float Vin = scaledVout / scalingRatio;
+
+    return Vin;
+}
+
+void ADC_select_channel(uint8_t channel) {
+    channel &= 0x0F; //Look at lowest 4 bits
+    ADMUX &= ~((1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (1 << MUX0)); //Clear channel
+    ADMUX |= channel;
+}
+
+void UART_print_measurements(float arr[NO_MEASUREMENTS]) {
+    UART_print("[");
+    UART_print_float_2dp(arr[0]);
+    UART_print(", ");
+    UART_print_float_2dp(arr[1]);
+    UART_print("]\r\n");
 }
 
 int main(void) {
     UART_init();
     setup_ADC();
 
-    while (1) {
-        uint16_t ADCreading = read_ADC();
-        float DC_Voltage = calculate_DC_voltage(ADCreading);
+    float measurements[NO_MEASUREMENTS];
+    uint16_t voltageReadingDC, currentReadingDC;
 
-        UART_print("ADC = ");
-        UART_print_uint(ADCreading);
+    while (1) {
+        //DC Voltage reading
+        ADC_select_channel(0);
+        read_ADC();//Throw away first reading
+        voltageReadingDC = read_ADC();
+        measurements[0] = calculate_DC_voltage(voltageReadingDC);
+
+        //DC Current reading
+        ADC_select_channel(1);
+        read_ADC();
+        currentReadingDC = read_ADC();
+        measurements[1] = calculate_DC_current(currentReadingDC);
+
+        //Printings
+        UART_print("Voltage ADC = ");
+        UART_print_uint(voltageReadingDC);
         UART_print("  Voltage = ");
-        UART_print_voltage_2dp(DC_Voltage);
-        UART_print(" V\r\n");
+        UART_print_float_2dp(measurements[0]);
+        UART_print(" V   ");
+
+        UART_print("Current ADC = ");
+        UART_print_uint(currentReadingDC);
+        UART_print("  Current = ");
+        UART_print_float_2dp(measurements[1]);
+        UART_print(" A\r\n");
+
+        UART_print_measurements(measurements);
+
+
     }
 
-    return EXIT_SUCCESS;
+    return 0;
 }
